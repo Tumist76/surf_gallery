5c43a6f56b9545480bdbe12860c248abf28af3a1
Dmitry <tuma98@gmail.com>
2022-07-24 23:35:08 +0700

    complete gallery view

--- app/src/main/java/ru/tumist/surfgallery/presentation/gallery/GalleryPageFragment.kt
+++ app/src/main/java/ru/tumist/surfgallery/presentation/gallery/GalleryPageFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.resources.MaterialResources.getDimensionPixelSize
 import com.google.android.material.snackbar.Snackbar
@@ -12,6 +14,7 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import ru.tumist.surfgallery.R
 import ru.tumist.surfgallery.databinding.FragmentGalleryPageBinding
 import ru.tumist.surfgallery.domain.model.PictureModel
+import ru.tumist.surfgallery.presentation.mainContainer.MainScreenContainerFragmentDirections
 import ru.tumist.surfgallery.presentation.mainScreen.state.MainScreenState
 import ru.tumist.surfgallery.presentation.mainScreen.viewModel.MainScreenSharedViewModel
 import ru.tumist.surfgallery.presentation.views.pictures.MarginItemDecoration
@@ -33,7 +36,11 @@ class GalleryPageFragment : Fragment() {
     }
 
     private fun onPictureItemTap(item: PictureModel) {
-
+        val arguments =
+            MainScreenContainerFragmentDirections.actionMainScreenContainerFragmentToPictureViewFragment(
+                item
+            )
+        findTopNavController().navigate(arguments)
     }
 
     override fun onCreateView(
@@ -53,16 +60,20 @@ class GalleryPageFragment : Fragment() {
 
     private fun stateListener(state: MainScreenState) {
         when (state) {
-            MainScreenState.Empty -> handleEmpty()
+            MainScreenState.Empty -> handleError()
             MainScreenState.Error -> handleError()
             is MainScreenState.Loaded -> handleLoaded(state)
-            else -> handleLoading()
+            is MainScreenState.Loading -> handleLoading(state)
+            else -> {}
         }
     }
 
     private fun initViews() {
         val layoutManager = GridLayoutManager(activity, 2)
+        binding.galleryLoadErrorView.isVisible = false
+        binding.galleryLoader.isVisible = true
         binding.picturesSwipeRefresh.setOnRefreshListener { sharedViewModel.getPictures() }
+        binding.galleryRefreshBtn.setOnClickListener { sharedViewModel.getPictures() }
         binding.searchIconIv.setOnClickListener {
             findTopNavController().navigate(R.id.searchScreenFragment)
         }
@@ -79,27 +90,30 @@ class GalleryPageFragment : Fragment() {
     }
 
     private fun handleLoaded(state: MainScreenState.Loaded) {
+        if (state.isCompletedWithError) {
+            Snackbar.make(
+                binding.root,
+                resources.getString(R.string.connection_error_snackbar_text),
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
         picturesAdapter.submitList(state.pictures)
         binding.picturesSwipeRefresh.isRefreshing = false
+        binding.galleryLoader.isVisible = false
+        binding.galleryLoadErrorView.isVisible = false
     }
 
-    private fun handleEmpty() {
-        Snackbar.make(
-            binding.root, "Пусто", Snackbar.LENGTH_LONG
-        ).show()
-    }
-
-
-    private fun handleLoading() {
-        Snackbar.make(
-            binding.root, "Грузимся", Snackbar.LENGTH_LONG
-        ).show()
+    private fun handleLoading(state: MainScreenState.Loading) {
+        if (state.pictures.isEmpty()) {
+            binding.galleryLoader.isVisible = true
+        }
+        picturesAdapter.submitList(state.pictures)
     }
 
     private fun handleError() {
-        Snackbar.make(
-            binding.root, "Ошибочка", Snackbar.LENGTH_LONG
-        ).show()
+        binding.galleryLoadErrorView.isVisible = true
+        binding.galleryLoader.isVisible = false
+        binding.picturesSwipeRefresh.isRefreshing = false
     }
 
     override fun onDestroyView() {
